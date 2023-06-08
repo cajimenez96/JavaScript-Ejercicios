80be61092d0b26e7706eabb5b68e851144c1a054
Carlos Jimenez <cjimenez@totalcoin.com>
2023-06-08 19:29:46 -0300

    Primer commit: Estructura y enunciado

---
+++ app/app.js
@@ -0,0 +1,13 @@
+/* Estructuras condicionales!!
+
+Vamos a crear nuestro app.js y vamos a tratar de resolver lo siguientes ejercicios aplicando el conocimiento que ya tenemos:
+
+1. Crear una aplicación la cual tendremos que crear 3 números, luego vamos a determinar cual es el mayor y/o si son iguales o no.
+2. Creamos una aplicación que determine si un determinado número es par o impar.
+3. Creamos una aplicación que determine si un determinado número es primo.
+4. Crear una aplicación que deduzca la calificación (A-F) de un alumno y emita el mensaje correspondiente de acuerdo a la nota ingresada:
+  A- Excelente
+  B- Buena
+  C- Regular
+  D- Suficiente
+  F- No Suficiente */
