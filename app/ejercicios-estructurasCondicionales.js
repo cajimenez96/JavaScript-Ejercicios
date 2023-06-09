100c00d3e246fa7e2bfad6db295c508940cf4a58
Gonzalo Cainzo <gcainzop@gmail.com>
2023-06-08 21:46:20 -0300

    ejercicio 1

--- app/ejercicios-estructurasCondicionales.js
+++ app/ejercicios-estructurasCondicionales.js
@@ -11,3 +11,15 @@ Vamos a crear nuestro app.js y vamos a tratar de resolver lo siguientes ejercici
   C- Regular
   D- Suficiente
   F- No Suficiente */
+
+  const numero1 = prompt ('ingrese un numero')
+  const numero2 = prompt ('ingrese un numero')
+  const numero3 = prompt ('ingrese un numero')
+
+if (numero1 > numero2 && numero1 > numero3)
+console.log ('la variable numero 1 es la mayor') {
+  else if (numero2 > numero1 && numero2 > numero3)
+  }
+  else if (numero3 > numero1 && numero3 > numero2) {
+    console
+  }
