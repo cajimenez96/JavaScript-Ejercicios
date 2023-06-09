d12d4732d1fcc8e0c121bf891a8a398184696569
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-06-09 01:07:34 -0300

    q

--- app/app.js
+++ app/app.js
@@ -12,15 +12,36 @@ Vamos a crear nuestro app.js y vamos a tratar de resolver lo siguientes ejercici
   D- Suficiente
   F- No Suficiente */
 
+
+  //TAREA 1 
+
 // let num1 = prompt("Ingrese el primer numero");
 // let num2 = prompt("Ingrese el segundo numero");
 // let num3 = prompt("Ingrese el tercer numero");
 
 // if(num1==num2==num3)console.log("Los numeros son iguales.");
 // else {console.log("El numero mas grande es: " + Math.max(num1,num2,num3))};
+  //TAREA 2
+// let num=parseInt(prompt("Ingrese un numero para saber si es par"));
 
-let num=parseInt(prompt("Ingrese un numero para saber si es par"));
+// if(num%2==0){
+//   console.log("Su numero es par");
+// }else console.log("Su numero es impar");
 
-if(num%2==0){
-  console.log("Su numero es par");
-}else console.log("Su numero es impar");
+  //TAREA 3
+
+let num=prompt("Ingrese un numero para saber si es Primo");
+let esPrimo = true;
+
+if(num<=1){
+  esPrimo=false;
+}else{
+  for(let div=2;div<num;div++){
+    if(num%div==0){
+      esPrimo=false;
+      break;
+    }
+  }
+}
+if(esPrimo)console.log("Su numero es primo.");
+else console.log("Su numero no es primo");
