f43e3b0a7ec9ba6b57e75ee2e5b86dbed923c9a1
matiasmaldoando <matiasjmaldo@gmail.com>
2023-06-13 13:53:33 -0300

    ejercicios 1,2 y 4

--- app/app.js
+++ app/app.js
@@ -11,3 +11,66 @@ Vamos a crear nuestro app.js y vamos a tratar de resolver lo siguientes ejercici
   C- Regular
   D- Suficiente
   F- No Suficiente */
+  
+
+////////// ejercicio 1 //////////////
+
+// const numero1 = prompt('ingresa un numero');
+// const numero2 = prompt('ingresa un numero');
+// const numero3 = prompt('ingresa un numero');
+
+// if (numero1 > numero2 && numero1 > numero3 ) {
+// console.log ('la variable numero1 es la mayor');}
+// else if (numero2 > numero1 && numero2 > numero3){
+// console.log ('la variable numero2 es la mayor');}
+// else if (numero3 > numero1 && numero3 > numero2){
+//   console.log ('la variable numero3 es la mayor');}
+// else {
+//   console.log ('los numeros son iguales');
+// }
+
+
+
+////////// ejercicio 2//////////////
+
+// let num1 = prompt('Ingresa un numero para saber si es par')
+// if (num1 % 2 == 0) {
+//   console.log ('la variable es par');
+// }
+// else {
+//   console.log ('la variable es impar');
+// }
+
+
+////////// ejercicio 4 //////////////
+
+// const calficacion = prompt('Ingresa una la letra inicial de calificacion').toUpperCase ();
+
+
+// if (calficacion === 'A') {
+//   console.log('Excelente');}
+
+// else if (calficacion === 'B') {
+//   console.log ('Bueno');}
+
+// else if (calficacion === 'C') {
+//   console.log ('Regular');}
+
+// else if (calficacion === 'D') {
+// console.log ('Suficiente');}
+
+// else if (calficacion === 'F') {
+//   console.log ('No suficiente');}
+
+// else {
+//   console.log ('la letra ingresada no es valida');
+// }
+
+
+
+
+ 
+ 
+
+
+
