482eaf15b85bbd8e40ac08c69b12298a02ce4ed8
agustintarancon <agustinbuf@gmail.com>
2023-06-15 11:28:22 -0300

    Ejercicio 2 con switch

--- app/app.js
+++ app/app.js
@@ -12,78 +12,25 @@ Vamos a crear nuestro app.js y vamos a tratar de resolver lo siguientes ejercici
   D- Suficiente
   F- No Suficiente */
 
-// Ejercicio 1.
-
-// const num1 = prompt("Ingresa el primer numero");
-// const num2 = prompt("Ingresa el segundo numero");
-// const num3 = prompt("Ingresa el tercer numero");
-
-// if (num1 > num2 && num1 > num3) {
-//   console.log("El primer numero es el mayor");
-// }
-// else if (num2 > num1 && num2 > num3) {
-//   console.log("El segundo numero es el mayor");
-// }
-// else if (num3 > num1 && num3 > num2) {
-//   console.log("El tercer numero es el mayor");
-// }
-// else {
-//  console.log("Los numeros ingresados son iguales");
-// }
-
-//////////////////////////////////////////////////////
+// EJERCICIOS CON SWITCH
 
 // Ejercicio 2.
 
-// const num = parseInt(prompt("Ingrese un numero"));
+let num = parseInt(prompt("Ingrese un numero del 1 al 5"));
 
-// if (num % 2 == 0) {
-//   console.log("El numero ingresado es par");
-// }
-// else {
-//   console.log("El numero ingresado es impar");
-// }
+switch (num) {
+  case 1:
+  case 3:  
+  case 5:
+    console.log("El numero es impar");  
+  break;
 
-//////////////////////////////////////////////////////
+  case 0:
+  case 2:
+  case 4:
+    console.log("El numero es par");    
+  break;
 
-// Ejercicio 3. 
-
-// let numero = prompt("ingrese un numero");
-// let divisores = 0;
-
-// for(let i = 1 ; i <= numero ; i++)
-
-// if (numero % i == 0) {
-//   divisores++;
-// }
-// if(divisores == 2){
-//   console.log("El numero ingresado es primo");
-// }
-// else{
-//   console.log("El numero ingresado no es primo");
-// }
-
-//////////////////////////////////////////////////////
-
-// Ejercicio 4.
-
-const nota = prompt("Ingrese su nota").toUpperCase();
-
-if (nota === "A") {
-  console.log("Excelente");
-}
-else if (nota === "B") {
-  console.log("Buena");
-}
-else if (nota === "C") {
-  console.log("Regular");
-}
-else if (nota === "D") {
-  console.log("Suficiente");
-}
-else if (nota === "F") {
-  console.log("No Suficiente");
-}
-else {
-  console.log("La nota ingresada no es valida");
+  default:
+    console.log("El numero ingresado es mayor a 5, por lo que no se puede evaluar");
 }
