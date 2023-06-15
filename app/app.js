c89d354a7021d5545baef535eb6b901b18678dac
agustintarancon <agustinbuf@gmail.com>
2023-06-15 12:03:21 -0300

    Ejercicio 4 con switch

--- app/app.js
+++ app/app.js
@@ -37,28 +37,50 @@ Vamos a crear nuestro app.js y vamos a tratar de resolver lo siguientes ejercici
 
 // Ejercicio 3 con switch.
 
-let numero = parseInt(prompt("Ingrese un numero del 1 al 10"));
+// let numero = parseInt(prompt("Ingrese un numero del 1 al 10"));
 
-switch (numero) {
-  case 2:
-  case 3:  
-  case 5:
-  case 7:
-    console.log("El numero es primo");  
-  break;
+// switch (numero) {
+//   case 2:
+//   case 3:  
+//   case 5:
+//   case 7:
+//     console.log("El numero es primo");  
+//   break;
 
-  case 0:
-  case 1:
-  case 4:
-  case 6:
-  case 8:
-  case 9:
-  case 10:
-    console.log("El numero no es primo");    
-  break;
+//   case 0:
+//   case 1:
+//   case 4:
+//   case 6:
+//   case 8:
+//   case 9:
+//   case 10:
+//     console.log("El numero no es primo");    
+//   break;
 
+//   default:
+//     console.log("El numero ingresado es mayor a 10, por lo que no se puede evaluar");
+// }
+
+// Ejercicio 4 con switch.
+
+let nota = prompt("Ingrese la nota").toUpperCase();
+
+switch (nota) {
+  case "A":
+    console.log("Excelente");
+  break
+  case "B":
+    console.log("Buena");
+  break
+  case "C":
+    console.log("Regular");
+  break
+  case "D":
+    console.log("Suficiente");
+  break
+  case "F":
+    console.log("No Suficiente");
+  break
   default:
-    console.log("El numero ingresado es mayor a 10, por lo que no se puede evaluar");
+    console.log("La nota ingresada no es valida");
 }
-
-
