de106a168e32b835eef64f0c5f04d7c73a737525
unknown <FrancoMarga>
2023-06-22 18:52:37 -0300

    Punto 1,2 y 3 bucles realizado (no pude realizar el commit en cada ejercicio me daba error (usuario y mail)  y no sabia lo que era pero ya solucione)

--- app/ejercicios-estructurasCondicionales.js
+++ app/ejercicios-estructurasCondicionales.js
@@ -11,3 +11,107 @@ Vamos a crear nuestro app.js y vamos a tratar de resolver lo siguientes ejercici
   C- Regular
   D- Suficiente
   F- No Suficiente */
+
+
+
+//-----------Ejercicio 1-------------
+
+
+/* 
+alert("Ingrese 3 números");
+let numero1=parseInt(prompt("Ingrese el primero"));
+let numero2=parseInt(prompt("Ingrese el segundo"));
+let numero3=parseInt(prompt("Ingrese el tercero"));
+
+if(numero1==numero2 && numero1==numero3){
+  console.log(`los 3 numeros ingresados son iguales`);
+
+}else if(numero1==numero2 && numero1>numero3){
+  console.log(`El primer (${numero1}) y el segundo (${numero2}) número ingresado son iguales y los mayores`);
+
+}else if(numero2==numero3 && numero2>numero1){
+  console.log(`El segundo (${numero2}) y el tercer (${numero3}) número ingresado son iguales y los mayores`);
+
+}else if(numero3==numero1 && numero3>numero2){
+  console.log(`El primer (${numero1}) y el tercer (${numero3}) número ingresado son iguales y los mayores`);
+
+}else if(numero1==numero2 && numero1<numero3){
+  console.log(`El tercer (${numero3}) número ingresado es el mayor;  el primero (${numero1}) y el segundo (${numero2}) son iguales`);
+
+}else if(numero1==numero3 && numero1<numero2){
+  console.log(`El segundo (${numero2}) número ingresado es el mayor;  el primero (${numero1}) y el tercero (${numero3}) son iguales`);
+
+}else if(numero2==numero3 && numero2<numero1){
+  console.log(`El primer (${numero1}) número eingresado s el mayor;  el segundo (${numero2}) y el tercero (${numero3}) son iguales`);
+
+}else if(numero1>numero2 && numero1>numero3){
+  console.log(`El primer número ingresado (${numero1}) es el mayor`);
+
+}else if(numero2>numero1 && numero2>numero3){
+  console.log(`El segundo número ingresado (${numero2}) es el mayor`);
+
+}else if(numero3>numero1 && numero3>numero2){
+  console.log(`El tercer número ingresado (${numero3}) es el mayor`);
+
+}
+*/
+
+
+
+
+//-----------Ejercicio 2-------------
+
+
+/*
+let numero=prompt("Ingrese un numero");
+
+if(numero%2==0){
+  console.log("El número ingresado es par");
+
+}else{
+  console.log("El número ingresado es impar");
+}
+
+*/
+
+
+
+//-----------Ejercicio 3-------------
+
+
+/*
+let numero=prompt("Ingrese un número");
+
+if(numero%numero==0 && numero%1==0){
+  console.log("El número ingresado es un numero primo");
+
+}else{
+  console.log("El número ingresado no es un numero primo");
+}
+*/
+
+
+
+//--------------Ejercicio 4----------------
+
+/*
+let nota=prompt("Ingrese una nota");
+nota=nota.toUpperCase();
+
+if(nota=="A"){
+  console.log(`El alumno sacó una nota Exelente(${nota})`);
+
+}else if(nota=="B"){
+  console.log(`El alumno sacó una nota buena(${nota})`);
+
+}else if(nota=="C"){
+  console.log(`El alumno sacó una nota Regular(${nota})`);
+
+}else if(nota=="D"){
+  console.log(`El alumno sacó una nota Suficiente(${nota})`);
+
+}else if(nota=="F"){
+  console.log(`El alumno sacó una nota No Suficiente(${nota})`);
+
+}¨
+*/
--- app/ejercicios-estructurasRepetitivas.js
+++ app/ejercicios-estructurasRepetitivas.js
@@ -15,5 +15,110 @@ EJERCICIOS CON CICLOS REPETITIVOSSS!!!
 
 2. Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
 
-3. Crear un programa que pida ingresar usuario y contraseña. El programa debe permitir hasta 3 intentos, cuando el usuario y/o la contraseña sean incorrectos. Si se ingresa correctamente ambos antes de 3 intentos erróneos, debe mostrar un mensaje de bienvenida. Si se ingresan 3 intentos erróneos, debe mostrar un mensaje de error y terminar el programa. El usuario y la contraseña ya deben estar guardadas en variables.
+3. Crear un programa que pida ingresar usuario y contraseña. El programa debe permitir hasta 3 intentos, cuando el usuario y/o la contraseña sean incorrectos. 
+Si se ingresa correctamente ambos antes de 3 intentos erróneos, debe mostrar un mensaje de bienvenida. Si se ingresan 3 intentos erróneos, debe mostrar un mensaje de error
+ y terminar el programa. El usuario y la contraseña ya deben estar guardadas en variables.
 */
+
+
+
+
+
+//-----------Ejercicio 1-------------
+
+/*
+let lineas=prompt("Ingrese la cantidad de líneas que desea no tiene que ser mayor de 50");
+
+while(lineas>50){
+  alert("Las lineas ingresadas superan el máximo permitido");
+  lineas=prompt("Ingrese nuevamente la cantidad de lineas");
+}
+
+for(let index=0;index<=lineas;index++){
+
+  for(let a=0;a<index;a++){
+    console.log("*");
+  }
+  console.log("\n");
+}
+*/
+
+
+
+
+
+
+//-----------Ejercicio 2-------------
+
+/*
+let numero=0;
+
+numero=parseInt(prompt("Ingrese un numero"));
+let numero2=Number(numero);
+
+while(!Number(numero)){
+    alert("El dato ingresado NO es un número");
+    numero=prompt("Ingrese un número");
+}
+
+if(confirm("Desea seguir escribiendo otro numero?")==true){
+    
+    do{
+        numero=prompt("Ingrese un número");
+        if(Number(numero)){
+            numero2=Number(numero2)+Number(numero);
+        }else{
+            while(!Number(numero)){
+                alert("El dato ingresado NO es un número");
+                numero=prompt("Ingrese nuevamente un número");
+            }
+            numero2=Number(numero2)+Number(numero);
+        }
+        
+    }while(confirm("Desea seguir escribiendo otro numero?")==true);
+
+    console.log(`la suma total de los numeros ingresados es igual a ${numero2}`);
+    
+}else{
+  console.log(`la suma total de los numeros ingresados es igual a ${numero2}`);
+}
+*/
+
+
+
+
+
+//-----------Ejercicio 3-------------
+
+let usuario="franco";
+let contraseña="0000";
+
+let usuarioIn=prompt("Ingrese el usuario");
+let contraseñaIn=prompt("Ingrese la contraseña");
+
+for(let index=2;index>=0;index--){
+
+  if(usuario==usuarioIn && contraseña==contraseñaIn){
+    alert("Bienvenido a nuestra app");
+    index=-1;
+
+  }else if(usuario==usuarioIn && contraseña!=contraseñaIn){
+    alert(`Contraseña incorrecta, ingrese nuevamente. (quedan ${index} intentos)`);
+    usuarioIn=prompt("Ingrese el usuario");
+    contraseñaIn=prompt("Ingrese la contraseña");
+
+  }else if(contraseña==contraseñaIn && usuario!=usuarioIn){
+    alert(`Usuario incorrecto, ingrese nuevamente. (quedan ${index} intentos)`);
+    usuarioIn=prompt("Ingrese el usuario");
+    contraseñaIn=prompt("Ingrese la contraseña");
+
+  }else if(usuario!=usuarioIn && contraseña!=contraseñaIn){
+    alert(`Usuario y contraseña incorrectos, ingrese nuevamente. (quedan ${index} intentos)`);
+    usuarioIn=prompt("Ingrese el usuario");
+    contraseñaIn=prompt("Ingrese la contraseña");
+  }
+    
+  if(index==0){
+    alert("ERROR - Maximos intentos superado. Intente de nuevo mas tarde")
+  }
+}
