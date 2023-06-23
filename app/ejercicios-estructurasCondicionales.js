a25e86cdab9f979a1242f6e42476c824fafd99c3
Gonzalo Cainzo <gcainzop@gmail.com>
2023-06-23 17:38:56 -0300

    Ejercicio nro 2 y 3 Resueltos

--- app/ejercicios-estructurasCondicionales.js
+++ app/ejercicios-estructurasCondicionales.js
@@ -24,19 +24,65 @@ Vamos a crear nuestro app.js y vamos a tratar de resolver lo siguientes ejercici
 //     console
 //   }
 
-let numero1 = parseInt(prompt ("ingrese un numero")) 
-let numero2 = parseInt(prompt ("ingrese un numero"))
-let numero3 = parseInt(prompt ("ingrese un numero"))
+// EJERCICIO 1 - DETERMINO CUAL ES EL NUMERO MAYOR Y SI SON IGUALES O NO
 
-if (numero1 > numero2 && numero1 > numero3) {
-  console.log ("numero 1 es mayor")
-}
-else if (numero2 > numero1 && numero2 > numero3) {
-  console.log ("numero 2 es mayor")
-}
-else if (numero3 > numero1 && numero3 > numero2) {
-  console.log ("numero 3 es mayor")
-}
-else 
-console.log ("los 3 numero ingresados son iguales")
+// let numero1 = parseInt(prompt ("ingrese un numero")) 
+// let numero2 = parseInt(prompt ("ingrese un numero"))
+// let numero3 = parseInt(prompt ("ingrese un numero"))
 
+// if (numero1 > numero2 && numero1 > numero3) {
+//   console.log ("numero 1 es mayor")
+// }
+// else if (numero2 > numero1 && numero2 > numero3) {
+//   console.log ("numero 2 es mayor")
+// }
+// else if (numero3 > numero1 && numero3 > numero2) {
+//   console.log ("numero 3 es mayor")
+// }
+// else { 
+// console.log ("los 3 numero ingresados son iguales") }
+
+
+
+// EJERCICIO NUMERO 2 - DETERMINAR SI ES PAR O IMPAR EL NRO INGRESADO
+// let numero = parseInt (prompt("ingrese un numero"))
+
+// if (numero%2==0) {
+// console.log ("Este numero es par")
+// }
+
+// else {
+//   console.log ("Este numero es impar")
+// }
+
+// PRUEBA EJERCICIO 3
+// let numero = parseInt(prompt("Ingrese un numero"))
+// let divisores=0 
+
+// for (let i=1 ; i <= numero ; i++) 
+// {  if (numero % i == 0) }
+
+// if (divisores==2){
+//   console.log("Es un numero primo")
+// }
+
+// else {
+//   console.log("No es un numero primo")
+// }
+
+
+// EJERCICIO NRO 4 - DETERMINAR SI UN NUMERO ES PRIMO O NO
+
+let numero = parseInt(prompt('ingrese un numero'));
+
+let divisores=0;
+
+for(let i =1 ; i <=numero ; i ++) {
+ if(numero % i  == 0)
+   divisores = divisores + 1
+}
+if(divisores==2)
+  console.log('es primo');
+else{
+  console.log('no primo');
+}
