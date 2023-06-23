ac1932a4cb0583b790767e14a5db50f02f480269
leandrocudmani <leandro.cudmani.tucu@gmail.com>
2023-06-23 11:57:45 -0300

    se realizo tarea 1 de estructuras repetitivas

---
+++ app/app.js
@@ -0,0 +1,31 @@
+/* Estructuras condicionales!!
+
+Vamos a crear nuestro app.js y vamos a tratar de resolver lo siguientes ejercicios aplicando el conocimiento que ya tenemos:
+
+1. Crear una aplicación la cual tendremos que crear 3 números, luego vamos a determinar cual es el mayor y/o si son iguales o no.
+2. Creamos una aplicación que determine si un determinado número es par o impar.
+3. Creamos una aplicación que determine si un determinado número es primo.
+4. Crear una aplicación que deduzca la calificación (A-F) de un alumno y emita el mensaje correspondiente de acuerdo a la nota ingresada:
+  A- Excelente
+  B- Buena
+  C- Regular
+  D- Suficiente
+  F- No Suficiente */
+
+
+//  TAREA 1
+  const numero1 = prompt('Ingresa un numero')
+  const numero2 = prompt('Ingresa un numero')
+  const numero3 = prompt('Ingresa un numero')
+
+  if(numero1 > numero2 && numero1 > numero3) {
+  console.log('la variable nnumero1 es la mayor');
+  } else if(numero2 > numero1 && numero2 > numero3){
+  console.log('la variable numero2 es la mayor');
+  } else if(numero3 > numero1 && numero3 > numero2){
+  console.log('la variable numero 3 es la mayor');
+  } else {
+  console.log('los numero son iguales');
+  }
+
+  // TAREA 2
--- app/ejercicios-estructurasRepetitivas.js
+++ app/ejercicios-estructurasRepetitivas.js
@@ -17,3 +17,10 @@ EJERCICIOS CON CICLOS REPETITIVOSSS!!!
 
 3. Crear un programa que pida ingresar usuario y contraseña. El programa debe permitir hasta 3 intentos, cuando el usuario y/o la contraseña sean incorrectos. Si se ingresa correctamente ambos antes de 3 intentos erróneos, debe mostrar un mensaje de bienvenida. Si se ingresan 3 intentos erróneos, debe mostrar un mensaje de error y terminar el programa. El usuario y la contraseña ya deben estar guardadas en variables.
 */
+
+//TAREA 1)
+let numero = parseInt(prompt('ingrese un numero'));
+for (let index = 0 ; index < numero; index++) {
+  let astericos = '*' + numero;
+  console.log(`${numero}`);
+}
