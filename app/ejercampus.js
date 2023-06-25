b0106b3e7b2b8912c5a5bc5731075423f43fc657
Mati <matirossi93@gmail.com>
2023-06-25 20:52:33 -0300

    ejercicio3-part2-campus

--- app/ejercampus.js
+++ app/ejercampus.js
@@ -66,31 +66,59 @@
 // }
 
 
-let nota = parseInt(prompt("Ingrese una nota del 1 al 10"));
-var calificacion;
+// let nota = parseInt(prompt("Ingrese una nota del 1 al 10"));
+// var calificacion;
 
-if (nota < 0 || nota > 10) {
-    alert("Numero erroneo");
+// if (nota < 0 || nota > 10) {
+//     alert("Numero erroneo");
 
-} else if (nota >= 0 && nota <=2) {
-    calificacion = "Muy deficiente.";
+// } else if (nota >= 0 && nota <=2) {
+//     calificacion = "Muy deficiente.";
 
-} else if (nota >= 3 && nota <=4) {
-    calificacion = "Insuficiente";
+// } else if (nota >= 3 && nota <=4) {
+//     calificacion = "Insuficiente";
 
-} else if (nota >= 5 && nota <= 6) {
-    calificacion = "Suficiente";
+// } else if (nota >= 5 && nota <= 6) {
+//     calificacion = "Suficiente";
 
-} else if (nota == 7) {
-    calificacion = "Bien";
+// } else if (nota == 7) {
+//     calificacion = "Bien";
 
-} else if (nota >= 8 && nota <= 9) {
-    calificacion = "Notable";
+// } else if (nota >= 8 && nota <= 9) {
+//     calificacion = "Notable";
 
-} else if (nota = 10){
-    calificacion = "Sobresaliente";
-} else {
-    alert("Ingrese un numero valido")
+// } else if (nota = 10){
+//     calificacion = "Sobresaliente";
+// } else {
+//     alert("Ingrese un numero valido")
+// }
+
+// alert("Su calificacion es " + calificacion);
+
+let cadenas = [];
+
+while (true) {
+    var confirmar = confirm("Deseas ingresar una cadena de texto?");
+
+    if (confirmar = false) {
+        break;
+    } 
+
+    var cadena = prompt("Ingrese una cadena de texto"); 
+
+    if (cadena === null) {
+        break;
+    } 
+
+    cadenas.push(cadena);
+
 }
 
-alert("Su calificacion es " + calificacion);
+if (cadenas.length > 0) {
+ var resultado = cadenas.join("-");
+ console.log("Cadenas concatenadas:", resultado);
+} else {
+    console.log("No se ingresaron cadenas");
+}
+
+
