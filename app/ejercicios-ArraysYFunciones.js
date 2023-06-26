b99f41d7a627e80d338cc5a3121843cdc4ab6fbe
EmilianoGasco <Emilianogasco@hotmail.com>
2023-06-26 13:59:45 -0300

    agregue funciones a todas las actividades

--- app/ejercicios-ArraysYFunciones.js
+++ app/ejercicios-ArraysYFunciones.js
@@ -13,44 +13,67 @@ TENER EN CUENTA QUE LAS RESOLUCIONES DE CADA EJERCICIO DEBEN ESTAR EN UNA FUNCIO
 
 
 //ACTIVIDAD 1
-// let colores = ['turquesa','verde','rojo','azul','morado','lila','negro','blanco','amarillo','rosa']
+function actividad1(){
+  let colores = ['turquesa','verde','rojo','azul','morado','lila','negro','blanco','amarillo','rosa']
 
-// for (let i = 0; i<colores.length;i++){
-//   console.log(colores[i])
-// }
-// console.log('------');
-// colores.sort();
-// for (let i = 0; i<colores.length;i++){
-//   console.log(colores[i])
-// }
-
-//ACTIVIDAD 2
-// let numeros = [];
-// for(i=0;i<20;i++){
-//   numeros.push(Math.floor(Math.random()*100));
-//   console.log(numeros[i]);
-// }
-
-//ACTIVIDAD 3
-// let numeros = [];
-// numeros.push(parseInt(prompt('ingrese un numero')))
-// console.log(numeros);
-
-//ACTIVIDAD 4
-let colores = ["azul", "amarillo", "rojo", "verde", "café", "rosa"];
-let colorUsuario = prompt("Introduce un color:");
-let encontrado = false;
-
-for (let i = 0; i < colores.length; i++) {
-  if (colores[i] === colorUsuario) {
-    encontrado = true;
-    break;
+  for (let i = 0; i<colores.length;i++){
+    console.log(colores[i])
+  }
+  console.log('---quí estan los colores ordenados ---');
+  colores.sort();
+  for (let i = 0; i<colores.length;i++){
+    console.log(colores[i])
   }
 }
 
-if (encontrado) {
-  console.log("El color está en el array.");
-} else {
-  console.log("El color no está en el array.");
+//ACTIVIDAD 2
+function actividad2(){
+let numeros = [];
+for(i=0;i<20;i++){
+  numeros.push(Math.floor(Math.random()*100));
+  console.log(numeros[i]);
 }
+}
+
+//ACTIVIDAD 3
+
+function actividad3(){
+  let numeros = [];
+  let limite = parseInt(prompt("Ingrese el limite de numeros"));
+
+  for (let i=0; i < limite; i++) {
+    numeros.push( Math.floor(Math.random() * 100));
+    console.log(numeros[i]);
+  }
+}
+
+//ACTIVIDAD 4
+function actividad4(){
+  let colores = ["azul", "amarillo", "rojo", "verde", "café", "rosa"];
+  let colorUsuario = prompt("Introduce un color:");
+  let encontrado = false;
+  
+  for (let i = 0; i < colores.length; i++) {
+    if (colores[i] === colorUsuario) {
+      encontrado = true;
+      break;
+    }
+  }
+  
+  if (encontrado) {
+    console.log("El color está en el array.");
+  } else {
+    console.log("El color no está en el array.");
+  }
+}
+
+
+
+function actividad5(){
+  let texto = prompt('ingrese varias palabras separadas por "coma" ');
+  let array = texto.split(',');
+  console.log(array)
+}
+
+
 
