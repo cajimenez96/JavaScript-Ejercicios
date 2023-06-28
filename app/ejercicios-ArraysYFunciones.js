54ee6ef4b5c6a5801c8e7a3d0e21db472150d3ea
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2023-06-28 16:39:11 -0300

    Resolucion de ejercicios de Array y funciones (1 al 5)

--- app/ejercicios-ArraysYFunciones.js
+++ app/ejercicios-ArraysYFunciones.js
@@ -10,3 +10,52 @@ Desarrollar los siguientes problemas:
 5. El usuario ingrese un string con varias palabras separadas por coma en un prompt y se deben convertir en un array, (el usuario ingresa: "1,2,3,4,5" y se convierte en [1,2,3,4,5]).
 
 TENER EN CUENTA QUE LAS RESOLUCIONES DE CADA EJERCICIO DEBEN ESTAR EN UNA FUNCION*/
+
+
+//EJERCICIO 1 
+
+function sortColors(){
+    const colors = ['red','blue','green','black','white','brown','purple','orange','grey','pink']
+    console.log(colors);
+    colors.sort();
+    console.log(colors);
+}
+
+//EJERCICIO 2
+
+function randomNumbers(){
+    let numbers = [];
+    for (let index = 0; index < 20; index++) {
+        numbers.push(Math.round(Math.random()*20));   
+    }
+    console.log(numbers);
+}
+
+//EJERCICIO 3
+
+function randomNumbers2() {
+  let numbers=[];
+  let n = parseInt(prompt());
+  for (let index = 0; index < n; index++) {
+    numbers.push(Math.round(Math.random()*100));   
+  }
+  return numbers;
+}
+//EJERCICIO 4
+function isThere(){
+  const colors=["azul", "amarillo", "rojo", "verde", "café", "rosa"];
+  let userColor = prompt('Ingrese un color');
+  let isThere = false;
+  colors.forEach(element => {
+    if(element===userColor){
+      isThere=true;
+    }
+  });
+  return isThere;
+}
+//EJERCICIO 5 
+function makeArray(){
+  let index = prompt('ingrese varias palabras separadas por "comas"');
+  return index.split(',');
+}
+console.log(makeArray());
