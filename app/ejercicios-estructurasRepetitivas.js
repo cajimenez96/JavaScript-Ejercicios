30c163b849e452f075884a53045d85d48ddd21ab
Gabriel Mendez <mendez.gabriel.d@hotmail.com>
2023-06-28 21:43:33 -0300

    resolucion de conflictos

--- app/ejercicios-estructurasRepetitivas.js
+++ app/ejercicios-estructurasRepetitivas.js
@@ -17,3 +17,21 @@ EJERCICIOS CON CICLOS REPETITIVOSSS!!!
 
 3. Crear un programa que pida ingresar usuario y contraseña. El programa debe permitir hasta 3 intentos, cuando el usuario y/o la contraseña sean incorrectos. Si se ingresa correctamente ambos antes de 3 intentos erróneos, debe mostrar un mensaje de bienvenida. Si se ingresan 3 intentos erróneos, debe mostrar un mensaje de error y terminar el programa. El usuario y la contraseña ya deben estar guardadas en variables.
 */
+
+/*---------Ejecicio N°1---------*/
+
+let cantAsteriscos = parseInt(prompt("Ingresa un numero entero"));
+
+Asteriscos = "";
+
+for(i=0 ; i<cantAsteriscos ; i++)
+{
+  Asteriscos = Asteriscos + "*";
+  console.log(Asteriscos);
+}
+
+
+
+if(cont === contMax) {
+  console.log("Se exedio el numero de intentos");
+}
